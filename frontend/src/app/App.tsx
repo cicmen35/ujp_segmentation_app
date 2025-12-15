4084a03c70cd25d1b47571617fde6f8654a1fe43
simonnemcic <simonnemci35@gmail.com>
2025-12-15 17:51:10 +0100

    initial commit with base project frontend structure

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../components/Header'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main className="mx-auto max-w-5xl px-4 py-12">
+        <h1 className="sr-only">UJP Segmentation App</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
