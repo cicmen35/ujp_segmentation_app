fbfb91311997c00e98e0cc048559818b163e16ff
cicmen35 <138312285+cicmen35@users.noreply.github.com>
2025-12-15 19:36:42 +0100

    (feat): added an upload box for images

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
+import { UploadDropzone } from '../features/upload/UploadDropzone'
 
 export function App() {
   return (
@@ -8,7 +9,9 @@ export function App() {
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
         <main className="flex-1 overflow-auto px-8 py-6">
-          <h1 className="sr-only">UJP Segmentation App</h1>
+          <div className="flex gap-10">
+            <UploadDropzone />
+          </div>
         </main>
       </div>
     </div>
